f103f958b4fa2f8fabde1819c7e6fb04b97d5f2e
Ricardo Werner <105825127+ricardo-werner@users.noreply.github.com>
2023-02-10 20:58:11 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById('calculamedia').onclick = function() {
+event.preventDefault();
+var nome = document.getElementById('nome').value;
+var nota1 = parseFloat(document.getElementById('nota1').value);
+var nota2 = parseFloat(document.getElementById('nota2').value);
+var nota3 = parseFloat(document.getElementById('nota3').value);
+
+var calculaMedia = (nota1 + nota2 + nota3 )/ 3;
+
+document.getElementById('mediaTotal').value = calculaMedia;
+}
